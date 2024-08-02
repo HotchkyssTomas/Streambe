0f6cdb1b8533cd052c29248730142037b7363372
HotchkyssTomas <tomashotchkyss@gmail.com>
2024-08-02 16:35:31 -0300

    Delete Streambe directory

--- Streambe/TP1 JavaScript/Variable.js
+++
@@ -1,5 +0,0 @@
-let uno = 5;    
-let dos = uno;  
-uno = 10;       
-
-console.log(dos); // Verificamos el valor de 'dos'
--- Streambe/TP1 JavaScript/caracteres.js
+++
@@ -1,5 +0,0 @@
-let texto = prompt("Por favor, ingresa un texto breve:");
-
-let cantidadDeCaracteres = texto.length;
-
-console.log("El texto ingresado tiene " + cantidadDeCaracteres + " caracteres.");
